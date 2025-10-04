0e43c36d2a5c8d18eded321b8e22ece67b3f0b97
Владимир Антипов <antipov.32@yandex.ru>
2025-10-04 23:37:54 +0300

    fix

--- app/database.py
+++ app/database.py
@@ -7,7 +7,10 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "sqlite+aiosqlite:///./cookbook.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)  # type: ignore
+async_session = sessionmaker(engine,
+                             expire_on_commit=False,
+                             class_=AsyncSession
+                             )  # type: ignore
 Base = declarative_base()
 
 
