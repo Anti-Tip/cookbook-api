486b95acdb61c0cf1c5370fd6b3a657b976bc1b1
Владимир Антипов <antipov.32@yandex.ru>
2025-10-04 23:28:16 +0300

    fix

--- app/database.py
+++ app/database.py
@@ -1,3 +1,5 @@
+from typing import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -5,10 +7,10 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "sqlite+aiosqlite:///./cookbook.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)  # type: ignore
 Base = declarative_base()
 
 
-async def get_db() -> AsyncSession:
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with async_session() as session:
         yield session
